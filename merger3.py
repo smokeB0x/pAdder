4cac37c346c9ba005e775c24b3b57897007ed738
aug9co <32621767+aug9co@users.noreply.github.com>
2022-04-27 10:05:15 +0200

    Update merger3.py

--- merger3.py
+++ merger3.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-#The script takes each word in passordliste.txt and adds the numbers in this script.  
+#The script takes each word in passord_temp0.txt and adds the numbers in this script.  
 #The output is saved to a file named passord_temp0.txt.txt
 import itertools
 
 #Add your digits to this list (leave the first one emty to also include one word without numbers)
-digits = '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '1996', '96', '24', '2405', '20', '2020', '1905', '0147', '1067', '1234', '123', '0557', '154'
+digits = '', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '123', '1234'
 with open('passord_temp0.txt') as file1:
     for line in file1:
         for c in itertools.product(digits, repeat=1):
